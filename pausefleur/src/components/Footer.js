c7831a4781e2b55b4c95bb8bee15a27741e50017
Vincent Linet <vincent.linet@epitech.eu>
2021-04-09 00:45:26 +0200

    Change luxury components

--- pausefleur/src/components/Footer.js
+++ pausefleur/src/components/Footer.js
@@ -1,30 +1,29 @@
 import React, { useState } from "react";
 import classnames from "classnames";
 
-import { AuthorIcon } from "./Icons";
+import { FlowerIcon } from "./Icons";
 import UnsplashLogo from "./logo/Unsplash";
 
-const renderAboutMe = className => (
+const renderDescription = (className) => (
   <div className={className}>
     <p>
-      Salut, je m'appelle Vincent, je ne suis pas doctorant, mais quelques uns de mes proches sont dans une phase de
-      rédaction et je souhaite les soutenir sans aucune contrainte, et vous invite à en fait de même grâce à Encourage
-      ton Thésard.
+      Salut, la période n'est pas facile et malgré tout le soutien moral de notre cher Ponce, il n'est pas toujours
+      évident de se mettre au boulot quand on a une lourde charge de travail.
     </p>
     <p>
-      J'aime utiliser ma créativité pour inventer divers genre de contenu pour le web, des sites, des illustrations et
-      des logos, et surtout mettre toute cette énergie dans des petites choses qui tendent à illuminer la vie de mon
-      entourage.
+      On espère donc avec quelques jolies images florales et quelques sourires animaliers vous transmettre la bonne
+      humeur et le courage dont vous aurez besoin pour avancer dans vos projets autant professionnels que dans votre vie
+      quotidienne. Des bisous !
     </p>
     <p>
-      <i>- À Amel</i>
+      <i>- Pour les belles fleurs de Ponpon</i>
     </p>
   </div>
 );
 
 const Footer = ({ credit, link, ...props }) => {
   const [hover, setHover] = useState(false);
-  const open = classnames("about-me", { open: hover });
+  const open = classnames("description", { open: hover });
 
   const handleMouseOver = () => setHover(true);
 
@@ -32,10 +31,10 @@ const Footer = ({ credit, link, ...props }) => {
 
   return (
     <div className="footer">
-      {renderAboutMe(open)}
+      {renderDescription(open)}
       <div className="author" onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
-        <AuthorIcon className="icon" height="36px" width="36px" />
-        Composé par&nbsp;<b>Vincent Linet</b>
+        <FlowerIcon className="icon" height="36px" width="36px" color="#FFFFFF" />
+        Composé par&nbsp;<b>des fleurs</b>&nbsp;pour&nbsp;<b>des fleurs</b>
       </div>
       <div className="credit">
         Photo de{" "}
